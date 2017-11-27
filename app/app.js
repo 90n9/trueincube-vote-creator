038704d95c1fb6892175afa2f02ce38602dd5437
Narathip Harijiratiwong <tholop@gmail.com>
2017-11-27 08:49:16 +0700

    update active link and scroll top

--- app/app.js
+++ app/app.js
@@ -2,11 +2,13 @@ angular.module('app', [
   'ui.router'
 ])
 .run(['$rootScope', '$state', '$stateParams', '$transitions', function($rootScope, $state, $stateParams, $transitions){
-  $transitions.onSuccess({}, function(transition) {
+  $transitions.onSuccess({}, function() {
     $rootScope.title = $state.current.title;
-    console.log('transition');
-    console.log(transition.params('form'));
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
+    if($state.current.name.indexOf("projectdetail") == -1){
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+    }else{
+      document.body.scrollTop = document.documentElement.scrollTop = $('main').offset().top;
+    }
   });
   $rootScope.$state = $state;
   return $rootScope.$stateParams = $stateParams;
